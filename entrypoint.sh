85e2233ad686854b682527764b9e9fa4872d6ca6
guessi <guessi@gmail.com>
2023-10-14 15:45:56 +0800

    Include "dnsperf --help" in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,49 @@
 DNSPERF_BINARY="/usr/local/bin/dnsperf"
 DNSPERF_RECORDS_INPUT="/opt/records.txt"
 
+# -----------------------------------------------------------------------------
+# Command output of "dnsperf --help"
+# -----------------------------------------------------------------------------
+#
+# DNS Performance Testing Tool
+# Version 2.13.1
+#
+# Usage: dnsperf [-f family] [-m mode] [-s server_addr] [-p port]
+#                [-a local_addr] [-x local_port] [-d datafile] [-c clients]
+#                [-T threads] [-n maxruns] [-l timelimit] [-b buffer_size]
+#                [-t timeout] [-e] [-E code:value] [-D]
+#                [-y [alg:]name:secret] [-q num_queries] [-Q max_qps]
+#                [-S stats_interval] [-u] [-B] [-v] [-W] [-h] [-H] [-O]
+#   -f address family of DNS transport, inet or inet6 (default: any)
+#   -m set transport mode: udp, tcp, dot or doh (default: udp)
+#   -s the server to query (default: 127.0.0.1)
+#   -p the port on which to query the server (default: udp/tcp 53, DoT 853 or DoH 443)
+#   -a the local address from which to send queries
+#   -x the local port from which to send queries (default: 0)
+#   -d the input data file (default: stdin)
+#   -c the number of clients to act as
+#   -T the number of threads to run
+#   -n run through input at most N times
+#   -l run for at most this many seconds
+#   -b socket send/receive buffer size in kilobytes
+#   -t the timeout for query completion in seconds (default: 5)
+#   -e enable EDNS 0
+#   -E send EDNS option
+#   -D set the DNSSEC OK bit (implies EDNS)
+#   -y the TSIG algorithm, name and secret (base64)
+#   -q the maximum number of queries outstanding (default: 100)
+#   -Q limit the number of queries per second
+#   -S print qps statistics every N seconds
+#   -u send dynamic updates instead of queries
+#   -B read input file as TCP-stream binary format
+#   -v verbose: report each query and additional information to stdout
+#   -W log warnings and errors to stdout instead of stderr
+#   -h print this help
+#   -H print long options help
+#   -O set long options: <name>=<value>
+#
+# -----------------------------------------------------------------------------
+
 MAX_TEST_SECONDS="${MAX_TEST_SECONDS:-60}"
 DNS_SERVER_ADDR="${DNS_SERVER_ADDR:-10.100.0.10}"
 DNS_SERVER_PORT=${DNS_SERVER_PORT:-53}
